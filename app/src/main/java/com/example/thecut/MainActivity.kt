1a535dec777b48cee760979bf22195fe563c80fc
Ruchirakavinda <ruchirakvnd@gmail.com>
2022-02-08 19:12:56 +0530

    splash screen02

--- app/src/main/java/com/example/thecut/MainActivity.kt
+++ app/src/main/java/com/example/thecut/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.thecut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.DecelerateInterpolator
+import androidx.core.content.ContextCompat
+import com.agrawalsuneet.dotsloader.loaders.LazyLoader
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
