360a682778c9171477a99d1c835273a435d9f710
Ruchirakavinda <ruchirakvnd@gmail.com>
2022-02-08 20:20:38 +0530

    splash screen animation

--- app/src/main/java/com/example/thecut/MainActivity.kt
+++ app/src/main/java/com/example/thecut/MainActivity.kt
@@ -2,16 +2,34 @@ package com.example.thecut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.DecelerateInterpolator
-import androidx.core.content.ContextCompat
-import com.agrawalsuneet.dotsloader.loaders.LazyLoader
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    //variables
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main)
+
+        //animations
+
+        var topAnimation : Animation = AnimationUtils.loadAnimation(this,R.anim.top_animation)
+        var bottomAnimation : Animation = AnimationUtils.loadAnimation( this,R.anim.bottom_animation)
+
+        var logo : ImageView = findViewById(R.id.logo)
+        var name : TextView = findViewById(R.id.name)
+        var nsbm : TextView = findViewById(R.id.nsbm)
+
+        logo.setAnimation(topAnimation)
+        name.setAnimation(bottomAnimation)
+        nsbm.setAnimation(bottomAnimation)
     }
 }
