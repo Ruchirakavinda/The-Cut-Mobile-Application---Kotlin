da72efb84e24ba03e8de5292a758a9a329bc4455
Ruchirakavinda <ruchirakvnd@gmail.com>
2022-02-28 10:59:37 +0530

    home page

--- app/src/main/java/com/example/thecut/LandingPage.kt
+++ app/src/main/java/com/example/thecut/LandingPage.kt
@@ -2,17 +2,10 @@ package com.example.thecut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
 
 class LandingPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing_page)
-
-        val toolbar : Toolbar = findViewById(R.id.toolbarNA)
-        setSupportActionBar(toolbar)
-        toolbar.setNavigationOnClickListener {
-            finish()
-        }
     }
 }
